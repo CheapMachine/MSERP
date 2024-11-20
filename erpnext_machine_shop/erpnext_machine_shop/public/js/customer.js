d7e854e5d3b71820a124beab77e132387febed4e
CheapMachine <Daniel@procut-cnc.com>
2024-11-20 03:06:02 -0600

    Delete erpnext_machine_shop directory

--- erpnext_machine_shop/erpnext_machine_shop/public/js/customer.js
+++
@@ -1,7 +0,0 @@
-erpnext.ui.form.on('Customer', {
-    validate: function(frm) {
-        if (!frm.doc.customer_name) {
-            frappe.throw(__('Please provide a customer name.'));
-        }
-    }
-});
