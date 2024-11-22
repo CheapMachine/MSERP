027e3509a6cb68782d2bf3d281f47c5f78839585
CheapMachine <Daniel@procut-cnc.com>
2024-11-21 21:28:45 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='erpnext_machine_shop',
-    version='1.0.0',
+    version='7.0.0',
     description='Custom ERPNext app for machine shop management',
     author='CheapMachine',
     author_email='support@cheapmachine.com',
